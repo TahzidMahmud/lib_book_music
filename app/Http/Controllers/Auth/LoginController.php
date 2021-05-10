7e5868c7b023594164ee2cd7534781d4c21e0297
TahzidMahmud <tahzid15-1272@diu.edu.bd>
2021-05-10 15:14:23 +0600

    user redirection after log in started

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Auth;
 
 class LoginController extends Controller
 {
@@ -26,7 +27,25 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
+    // protected $redirectTo = RouteServiceProvider::HOME;
+
+
+    public function redirectTo() {
+
+        $user = Auth::user();
+
+        switch(true) {
+            case $user->type=='vendor':
+                return '/vendor-dashboard';
+            case $user->type=='customer':
+                return '/';
+            case $user->type=='admin':
+                return '/admin-dashboard';
+            default:
+                return back();
+        }
+    }
+
 
     /**
      * Create a new controller instance.
