a9f888a66c3a322a179823bdb3580e40a26154c5
TahzidMahmud <tahzid15-1272@diu.edu.bd>
2021-05-20 08:38:27 +0600

    landing to controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -20,7 +20,7 @@ class HomeController extends Controller
         $this->genres=[
             "Action and adventure","Art/architecture","Alternate history","Autobiography","Anthology","Biography","Chick lit","Business/economics","Children's","Crafts/hobbies","Classic","Cookbook","Comic book","Diary","Coming-of-age","Dictionary","Crime","Encyclopedia","Drama","Guide","Fairytale","Health/fitness","Fantasy","History","Graphic novel","Home and garden","Historical fiction","Humor","Horror","Journal","Mystery","Math","Paranormal romance","Memoir","Picture book","Philosophy","Poetry","Prayer","Political thriller","Religion", "spirituality" ,"new age","Romance","Textbook","Satire","True crime","Science fiction","Review","Short story","Science","Suspense","Self help","Thriller","Sports and leisure","Western","Travel","Young adult","True crime"
         ];
-        $this->middleware('auth');
+        // $this->middleware('auth');
     }
 
 
@@ -32,6 +32,10 @@ class HomeController extends Controller
     public function index()
     {
         return view('home');
+    }
+    public function landing(){
+    return view('front-end.landing');
+
     }
     public function books(){
         $books=Book::all();
--- routes/web.php
+++ routes/web.php
@@ -16,9 +16,9 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/', function () {
 //     return view('welcome');
 // });
-Route::get('/', function () {
-    return view('front-end.landing');
-});
+
+Route::get('/','HomeController@landing')->name('landing');
+
 Route::get('/books', function () {
     return view('front-end.productsView');
 })->name('books');
